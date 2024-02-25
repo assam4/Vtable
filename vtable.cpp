e7083f1fd3def70458a7e605e37a6ed860168002
assam4 <115891697+assam4@users.noreply.github.com>
2024-02-26 01:19:22 +0400

    Update vtable.cpp

--- vtable.cpp
+++ vtable.cpp
@@ -2,103 +2,90 @@
 #include <map>
 #include <functional>
 
-using namespace std;
-
-//Base  class declaration
-
+//  Base  class declaration
   class Base
 {
  public:
-        Base():type('B') 
-                        {
-                           if(vtable.find(type)==vtable.end())
-                          vtable[type] = [this]() { f_declaration() ; } ;
-                        }
-
-        Base(const Base& oth):type('B')  {}
-
-        Base& operator= (const Base& oth)
-                        {  if(this == &oth ) return *this ;
-                           return *this ;
-                        }
-
-        void f_get() 
-                        {
-                          vtable[type]();
-                        }
+        Base()  :  type('B') 
+        {
+          if( vtable.find(type) == vtable.end() )
+          vtable[type] = [this]() { f_declaration(); };
+        }
+        virtual ~Base() noexcept = default ;
+        Base(const Base& oth) : type('B')  
+        { }
+        Base& operator= (const Base& other) noexcept
+        {  
+            if(this != &other )
+                type = 'B' ; 
+            return *this ;
+        }
+        void f_get() const 
+        {
+          vtable[type]() ;
+        }
  protected:
-
-     char type ;
-     static map< char , std::function<void()>> vtable;
-
+        char type ;
+        static std::map< char , std::function< void() >> vtable;
  private:
-
-    void f_declaration() { cout<<"Base::f"<<endl;}  
+        void f_declaration() const noexcept
+        { 
+          std::cout << "Calling Base::f()" << std::endl ;
+        }  
 };
 
-// static member definition
-map<char, std::function<void()>> Base::vtable;
+// static member global definition ( c++ rule for static)
+std::map< char , std::function< void() >> Base::vtable ;
 
-//Derived class declaration
-
+// Derived class declaration
 class Derived:public Base
 {
-public:
-       Derived():Base() 
-                        {
-                          type = 'D' ;
-                            if(vtable.find(type)==vtable.end())
-                          vtable[type] = [this]() { f_declaration();};
-                        }
-
-         Derived(const Derived& oth): Base(oth)
-                        {
-                           type = 'D' ;  
-                        }
-
-
+  public:
+       Derived(): Base() 
+        {
+          type = 'D' ;
+          if(vtable.find(type)==vtable.end())
+          vtable[type] = [this]() { f_declaration();};
+        }
+        Derived(const Derived& oth): Base(oth)
+        {
+          type= 'D' ;  
+        }
         Derived(const Base& oth)
-                        {
-                           type = 'D' ;
-                            if(vtable.find(type)==vtable.end())
-                           vtable[type] = [this]() { f_declaration();};
-                         }
-
-         Derived& operator= (const Base& oth)
-                        {  if(this == &oth ) return *this ;
-                           type = 'D' ;
-                           return *this ;
-                        }
-
-private:
-
- void f_declaration()
-                      { 
-                        cout<<"Derived::f"<<endl;
-                      }
+         {
+            type = 'D' ;
+            if(vtable.find(type) == vtable.end())
+            vtable[type] = [this]() { f_declaration(); };
+          }
+        Derived& operator= (const Base& other) noexcept
+          {  
+            if(this != &other )
+                type = 'D' ; 
+            return *this ;
+          }
+  private:
+   void f_declaration() const noexcept
+    { 
+      std::cout << "Calling Derived::f() " << std::endl;
+    }
 };
 
 int main()
-{
-    
+{ 
+  try
+    {
     Base obj1;
     obj1.f_get();
     Derived obj2;
     obj2.f_get();
     Base* ptr = &obj2;
     ptr->f_get();
-
     obj2 = obj1;
     obj2.f_get();
     Derived obj3(obj2);
     obj3.f_get();
-    
-
-
-
-    
-    
-
     return 0;
+    }
+    catch (...) { return 1 ; }
 }
 
